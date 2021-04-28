943d43679df8c0a846c3e902c227eac40e8e4244
Jordan Haywood <51302288+JordanHay@users.noreply.github.com>
2021-04-28 12:38:48 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CalculationsChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Temperature temperature = new Temperature();
+
+            for (int i = 0; i < 100; i++)
+            {
+                temperature.Insert(rnd.Next(1, 101));
+            }
+
+            Console.WriteLine($"Min Temp: {temperature.MinimumTemperature}");
+            Console.WriteLine($"Max Temp: {temperature.MaximumTemperature}");
+            Console.WriteLine($"Avg Temp: {temperature.AverageTemperature}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Temperature.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CalculationsChallenge
+{
+    public class Temperature
+    {
+
+
+            private List<int> temperatures = new List<int>();
+            public int MinimumTemperature { get; set; }
+            public int MaximumTemperature { get; set; }
+            public double AverageTemperature { get; set; }
+
+        public void Insert(int temperature)
+        {
+            temperatures.Add(temperature);
+            MinimumTemperature = temperatures.Min();
+            MaximumTemperature = temperatures.Max();
+            AverageTemperature = temperatures.Average();
+        }
+        
+    
+    }
+}
